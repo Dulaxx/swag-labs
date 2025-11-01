0b17f522b5f49a35a9a67b835b5d1137b78857cf
User <tharidukad@gmail.com>
2025-11-01 20:31:56 +0100

    review changes

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -2,15 +2,18 @@ import {test as setup, expect} from '@playwright/test';
 import {LoginPage} from '../pages/LoginPage/LoginPage';
 import path from 'path';
 
-const username = process.env.VALID_USERNAME as string;
-const password = process.env.VALID_PASSWORD as string;
+const username = process.env.VALID_USERNAME;
+const password = process.env.VALID_PASSWORD;
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
+
+if (!username || !password) {
+    throw new Error('VALID_USERNAME and VALID_PASSWORD must be set in environment variables');
+}
 
 setup('authentication', async ({page}) => {
     const loginPage = new LoginPage(page);
     await loginPage.navigateToLoginPage();
     await loginPage.loginToApplication(username, password);
-    console.log(username, password);
     await loginPage.verifyLoginSuccessful();
     await page.context().storageState({ path: authFile });
 })
