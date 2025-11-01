13e3744927f7790e36f7c0973df2f5f406bb7fb1
User <tharidukad@gmail.com>
2025-11-01 13:39:50 +0100

    add before for tests

--- tests/test-login.spec.ts
+++ tests/test-login.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect } from '@playwright/test';
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/inventory.html');
+});
+
 test('test one', async ({ page }) => {
   await page.locator('[data-test="inventory-item-description"]').first().click();
   await page.locator('[data-test="item-4-title-link"]').click();
