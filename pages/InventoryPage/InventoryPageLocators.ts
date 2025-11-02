5dd29318bee79317e742c84ce8f145ef3c36f8a0
User <tharidukad@gmail.com>
2025-11-02 21:37:56 +0100

    add test

--- pages/InventoryPage/InventoryPageLocators.ts
+++ pages/InventoryPage/InventoryPageLocators.ts
@@ -1,5 +1,5 @@
 export const locators = {
     inventoryItemPrice: "[data-test='inventory-item-price']",
-    addToCartButton: "[data-test='add-to-cart']",
+    addToCartButton: "[data-test='add-to-cart-sauce-labs-backpack']",
     shoppingCartLink: "[data-test='shopping-cart-link']",
 }
